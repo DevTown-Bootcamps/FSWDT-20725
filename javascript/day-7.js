0daae84712460c944e0088dc231d867673151832
[200~anshulojha1214@gmail.com <anshulojha1214@gmail.com>
2025-08-12 20:46:40 +0530

    variable

---
+++ javascript/day-7.js
@@ -0,0 +1,34 @@
+// js variables will take all possible values
+
+// let- values can be changed but it cannot be accessed outside of its scope
+// {
+// let a=10;
+// let b="Hello";
+// a=20;
+// console.log(a);
+// console.log(b);
+// }
+// console.log(a);
+// console.log(b);
+
+// var-you can change the value and access it outside of the scope
+// {
+// var a=10;
+// var b="Hello";
+// a=20;
+// console.log(a);
+// console.log(b);
+// }
+// console.log(a);
+// console.log(b);
+
+// const-you cannot chnage the value as well you cannot access it outside of the scope
+{
+const a=10;
+const b="Hello";
+// a=20;
+console.log(a);
+console.log(b);
+}
+console.log(a);
+console.log(b);
