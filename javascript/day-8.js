14f19779483dd030831c7ea76a8e58ba52842393
[200~anshulojha1214@gmail.com <anshulojha1214@gmail.com>
2025-08-14 20:14:09 +0530

    datatypes

---
+++ javascript/day-8.js
@@ -0,0 +1,45 @@
+// Datatypes:
+// primitive data types
+
+// String
+
+// String is combination of alphabets,numbers,special character
+
+// let name="Anshul23242@#$";
+
+// number
+
+// let age=30;
+
+//boolean-true or false
+// let check=true;
+// check=false;
+
+// undefined
+
+// let a;
+// console.log(a);
+
+// null value
+
+// let a=null;
+// console.log(a);
+
+// Object-collection of primitive datatypes
+
+// let person={
+//     name:'Anshul',
+//     age:22,
+//     check:false
+// }
+
+// console.log(person.name);
+
+// arrays
+// collection of multiple datatypes under same
+
+let fruit=["cherry","apple","pineapple",34,87,false];
+// index always starts with 0
+
+console.log(fruit);
+console.log(fruit[5]);
