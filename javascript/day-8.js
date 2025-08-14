90584e78cc86013d8d508094036565eb77b58ab1
[200~anshulojha1214@gmail.com <anshulojha1214@gmail.com>
2025-08-14 20:35:52 +0530

    comparative

--- javascript/day-8.js
+++ javascript/day-8.js
@@ -38,8 +38,70 @@
 // arrays
 // collection of multiple datatypes under same
 
-let fruit=["cherry","apple","pineapple",34,87,false];
+// let fruit=["cherry","apple","pineapple",34,87,false];
 // index always starts with 0
 
-console.log(fruit);
-console.log(fruit[5]);
+// console.log(fruit);
+// console.log(fruit[5]);
+
+// Define a array of string and print the third value out of it
+//Define s student object add, student_name,class,section,total_marks, and them seperately.
+
+
+// operators
+
+// assignment->=
+
+// let a=43;
+
+// Arithmetic operator
+
+// let b=20;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+
+// b=a+b;
+// b=b*a;
+
+// short hand operators
+
+// a=a+b;
+// a+=b;
+
+// a=a-b;
+// a-=b;
+
+// a=a*b;
+// a*=b;
+
+// a=a/b;
+// a/=b;
+
+
+// Equality
+// let a=10;
+// let b="10";
+
+
+// loose equality
+// console.log(a==b);
+
+// Strong/strict equalty
+// console.log(a===b);
+
+
+// comparison operators
+let a=40;
+let b=40;
+
+console.log(a>b);
+console.log(a<b);
+console.log(a<=b);
+console.log(a>=b);
+
+
