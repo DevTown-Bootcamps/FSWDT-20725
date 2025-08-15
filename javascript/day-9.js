7bed9c7a5a9f65ab28cf59d6004033dc3e6f466b
[200~anshulojha1214@gmail.com <anshulojha1214@gmail.com>
2025-08-15 18:16:05 +0530

    js

---
+++ javascript/day-9.js
@@ -0,0 +1,49 @@
+// Logical operators-combine two or more conditional operators
+
+let a=20;
+let b=20;
+let c=10;
+// if(a===b); if(b>a)
+// AND Operator
+// true+true=true;
+// true+false=false;
+// false+true=false;
+// false+false=false;
+// console.log(a>b && a>c);
+// console.log(a>b && a<c);
+// console.log(a<b && a>c);
+// console.log(a<b && a<c);
+
+
+// OR operator
+// true+true=true;
+// true+false=true;
+// false+true=true;
+// false+false=false;
+// console.log(a>b || a>c);
+// console.log(a>b || a<c);
+// console.log(a<b || a>c);
+// console.log(a<b || a<c);
+
+
+// if else condition
+
+// if(a>20){
+//     a-=10;
+// }else{
+//     a+=10;
+// }
+
+// console.log(a);
+
+// ternary operators
+
+let s=30;
+let g=20;
+let d=50;
+let e='50';
+
+let result=s<g? true:false;
+
+console.log(result);
+
