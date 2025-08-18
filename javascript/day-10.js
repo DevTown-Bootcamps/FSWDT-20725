71b6e70f9a958b3cac4dd55a02322b0070179c10
[200~anshulojha1214@gmail.com <anshulojha1214@gmail.com>
2025-08-18 18:09:40 +0530

    for

---
+++ javascript/day-10.js
@@ -0,0 +1,24 @@
+// Loop in js
+
+// for loop
+
+//for(intialization,condition,updation)
+//a++,++a; a=a+1;
+//i=0;true,i=1;
+//i=1;true;i=2;
+//i=2;true;i=3;
+//i=3;true;i=4;
+//i=4;true;i=5;
+//i=5;false; terminate=loop execution will stop here.
+
+
+// for(let i=0;i<5;i++){
+//     console.log(i);
+// }
+
+let fruit=["apple","banana","pineapple"];
+
+for(let i=0;i<fruit.length;i++){
+    console.log(fruit[i]);
+}
+
