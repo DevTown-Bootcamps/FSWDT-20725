6578b6094d404de894c17397fffc4e2526254a77
[200~anshulojha1214@gmail.com <anshulojha1214@gmail.com>
2025-09-23 18:38:32 +0530

    fs

--- nodejs/day-20.js
+++ nodejs/day-20.js
@@ -1,7 +1,29 @@
 //local module
 
-const math=require("./Math");
+// const math=require("./Math");
 
-console.log(math.add(3,2));
-console.log(math.check(4));
-console.log(math.mul(2,3));
+// console.log(math.add(3,2));
+// console.log(math.check(4));
+// console.log(math.mul(2,3));
+
+const fs=require('fs');
+
+//writing inside a file.
+
+// fs.writeFileSync('test.txt','replace the text');
+
+// fs.writeFile('test.txt',"I am writing inside the file",err=>{
+//     if(err) throw err;
+//     console.log("File written");
+// })
+
+//appending into a file
+// fs.appendFile('test.txt',"\n Append the new text",err=>{
+//     if(err) throw err;
+//     console.log("Text has been appended");
+// })
+
+//reading the file
+
+const data=fs.readFileSync('test.txt','utf-8');
+console.log(data);
