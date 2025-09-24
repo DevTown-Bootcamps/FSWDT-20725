abc60abbfde64191180cb6f3142d1a04a3ec918a
[200~anshulojha1214@gmail.com <anshulojha1214@gmail.com>
2025-09-24 18:48:10 +0530

    express server

--- nodejs/day-21.js
+++ nodejs/day-21.js
@@ -23,16 +23,37 @@
 // "/"-localhost:3000;
 // /about-localhost:3000/about
 
-const http=require('http');
+// const http=require('http');
 
-http.createServer((req,res)=>{
-    if(req.url==="/"){
-        res.end("Home page");
-    }else if(req.url==="/about"){
-      res.end("about");
-    }else{
-        res.end("Page not found");
-    }
-}).listen(3000,()=>{
-    console.log("Server is running on port 3000");
+// http.createServer((req,res)=>{
+//     if(req.url==="/"){
+//         res.end("Home page");
+//     }else if(req.url==="/about"){
+//       res.end("about");
+//     }else{
+//         res.end("Page not found");
+//     }
+// }).listen(3000,()=>{
+//     console.log("Server is running on port 3000");
+// })
+
+
+//express server
+
+const express=require('express');
+const app=express();
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to the home page");
 })
+
+app.get("/about",(req,res)=>{
+    res.send("Welcome to about page");
+})
+
+app.get("/contact",(req,res)=>{
+    res.send("Contact");
+})
+
+app.listen(8080, ()=>{ console.log("Server running on port 8080")});
+
